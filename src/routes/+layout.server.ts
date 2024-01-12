8118d5b130d1075250a9ebdf52edc18b9f8228bd
Artifex <mac@mkra.dev>
2024-01-12 09:24:18 +0800

    fix: page.server > layout.server

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,8 +1,8 @@
 import { getDirectusInstance } from '$lib/directus';
 import { readItems } from '@directus/sdk';
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ fetch }) => {
+export const load: LayoutServerLoad = async ({ fetch }) => {
 	const directus = await getDirectusInstance(fetch);
 
 	return {
