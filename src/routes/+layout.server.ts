1bd75baeba956e3c82743908d14055e30f65f71c
Artifex <mac@mkra.dev>
2024-01-15 19:30:00 +0800

    fix: removed logging meta

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,14 +3,11 @@ import { readItems } from '@directus/sdk';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ fetch }) => {
-	// const directus = await getDirectusInstance(fetch);
-
 	const meta = await getDirectusInstance(fetch).then((client) => {
 		return client.request<{ title: string; description: string; image: string }>(
 			readItems('global')
 		);
 	});
-	console.log(meta);
 	return {
 		meta
 	};
