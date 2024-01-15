00dacb31800a2d613f2d9a2f57426ce78e147603
Artifex <mac@mkra.dev>
2024-01-15 21:00:39 +0800

    fix: migration script

--- migrate.ts
+++ migrate.ts
@@ -1,19 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import fetch from 'cross-fetch';
+import 'dotenv/config';
 
-const BASE_DIRECTUS_URL = '';
-const BASE_ACCESS_TOKEN = '';
+const BASE_URL = process.env.BASE_URL ?? '';
+const BASE_ACCESS_TOKEN = process.env.BASE_ACCESS_TOKEN ?? '';
 
-const TARGET_DIRECTUS_URL = '';
-const TARGET_ACCESS_TOKEN = '';
+const TARGET_URL = process.env.TARGET_URL ?? '';
+const TARGET_ACCESS_TOKEN = process.env.TARGET_ACCESS_TOKEN ?? '';
 
 async function getSnapshot() {
-	const URL = `${BASE_DIRECTUS_URL}/schema/snapshot?access_token=${BASE_ACCESS_TOKEN}`;
+	const URL = `${BASE_URL}/schema/snapshot?access_token=${BASE_ACCESS_TOKEN}`;
 	const { data } = await fetch(URL).then((r) => r.json());
 	return data;
 }
 async function getDiff(snapshot: any) {
-	const URL = `${TARGET_DIRECTUS_URL}/schema/diff?access_token=${TARGET_ACCESS_TOKEN}`;
+	const URL = `${TARGET_URL}/schema/diff?access_token=${TARGET_ACCESS_TOKEN}`;
 
 	const { data } = await fetch(URL, {
 		method: 'POST',
@@ -27,7 +28,7 @@ async function getDiff(snapshot: any) {
 }
 
 async function applyDiff(diff: any) {
-	const URL = `${TARGET_DIRECTUS_URL}/schema/apply?access_token=${TARGET_ACCESS_TOKEN}`;
+	const URL = `${TARGET_URL}/schema/apply?access_token=${TARGET_ACCESS_TOKEN}`;
 
 	await fetch(URL, {
 		method: 'POST',
@@ -38,6 +39,7 @@ async function applyDiff(diff: any) {
 	});
 }
 
+// NOTE: This will only migrate schema defenitions, collections must be created and migrated manually
 async function main() {
 	const snapshot = await getSnapshot();
 	const diff = await getDiff(snapshot);
