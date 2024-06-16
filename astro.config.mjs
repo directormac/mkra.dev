586040898b512e54c055d350eb273dc19c860f66
Artifex <mac@mkra.dev>
2024-06-16 17:03:35 +0800

    feat: add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,39 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import sitemap from '@astrojs/sitemap'
-import { remarkReadingTime } from './src/utils/remarkReadingTime.ts'
-import remarkUnwrapImages from 'remark-unwrap-images'
-import rehypeExternalLinks from 'rehype-external-links'
-import expressiveCode from 'astro-expressive-code'
-import { expressiveCodeOptions } from './src/site.config'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import { remarkReadingTime } from './src/utils/remarkReadingTime.ts';
+import remarkUnwrapImages from 'remark-unwrap-images';
+import rehypeExternalLinks from 'rehype-external-links';
+import expressiveCode from 'astro-expressive-code';
+import { expressiveCodeOptions } from './src/site.config';
+import icon from 'astro-icon';
 
 // import vercel from '@astrojs/vercel/serverless'
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://mkra.dev',
-	integrations: [
-		expressiveCode(expressiveCodeOptions),
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap(),
-		mdx(),
-		icon()
-	],
-	markdown: {
-		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
-		rehypePlugins: [
-			[
-				rehypeExternalLinks,
-				{
-					target: '_blank',
-					rel: ['nofollow, noopener, noreferrer']
-				}
-			]
-		],
-		remarkRehype: {
-			footnoteLabelProperties: {
-				className: ['']
-			}
-		}
-	},
-	prefetch: true
-	// output: 'server'
-	// adapter: vercel({
-	// 	webAnalytics: { enabled: true }
-	// })
-})
+  site: 'https://mkra.dev',
+  integrations: [expressiveCode(expressiveCodeOptions), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), mdx(), icon(), svelte()],
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank',
+      rel: ['nofollow, noopener, noreferrer']
+    }]],
+    remarkRehype: {
+      footnoteLabelProperties: {
+        className: ['']
+      }
+    }
+  },
+  prefetch: true
+  // output: 'server'
+  // adapter: vercel({
+  // 	webAnalytics: { enabled: true }
+  // })
+});
