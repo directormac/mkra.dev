a888c9a03ae013d5bbdde967cb961a535644c56d
Artifex <mac@mkra.dev>
2024-06-17 10:48:56 +0800

    feat: description

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,9 +4,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'Artifex',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'My little space of existence',
+	title: 'Portfolio',
 	// Meta property used as the default description meta property
-	description: '',
+	description: 'My little space of existence on the web',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-US',
 	// Meta property, found in src/components/BaseHead.astro L:42
